c4be0ebc751c10bcf8e423251914dbe5317021d6
swl1008 <572514352@qq.com>
2019-02-28 19:14:10 +0800

    swl 02-28 19:13

---
+++ login/src/main/java/tech/wd/com/login/LoginActivity.java
@@ -0,0 +1,35 @@
+package tech.wd.com.login;
+
+
+import android.os.Bundle;
+
+import tech.wd.com.common.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void success(Object object) {
+
+    }
+
+    @Override
+    protected void failed(String error) {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_login;
+    }
+}
---
+++ login/src/main/java/tech/wd/com/login/RegisterActivity.java
@@ -0,0 +1,40 @@
+package tech.wd.com.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tech.wd.com.common.base.BaseActivity;
+
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void success(Object object) {
+
+    }
+
+    @Override
+    protected void failed(String error) {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}

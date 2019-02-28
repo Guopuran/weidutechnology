ecaa61621ada6cc3acf635caa94e1383f90dcc94
liuguangyan <2451528553@qq.com>
2019-02-28 08:37:39 +0800

    yan

--- information/src/main/java/tech/wd/com/information/fragment/InformationFragment.java
+++ information/src/main/java/tech/wd/com/information/fragment/InformationFragment.java
@@ -1,7 +1,15 @@
 package tech.wd.com.information.fragment;
 
 import android.support.v4.app.Fragment;
-
+/**
+ *
+ *@作者 刘
+ *
+ *@描述 资讯的fragment
+ *
+ *@时间 2019/2/27 16:37
+ *
+ */
 public class InformationFragment extends Fragment {
 
 }

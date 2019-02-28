4ecff45a592b00de7a438b1d6a984c57e8470baa
guopuran <www.1174965274@qq.com>
2019-02-28 09:44:56 +0800

    一

---
+++ app/src/main/java/com/wd/tech/AsdActivity.java
@@ -0,0 +1,35 @@
+package com.wd.tech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tech.wd.com.common.base.BaseActivity;
+
+public class AsdActivity extends BaseActivity {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void success(Object object) {
+
+    }
+
+    @Override
+    protected void failed(String error) {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_asd;
+    }
+}

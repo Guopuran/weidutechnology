641c249ebe0681964edaaf45877d466ff6322f9e
guopuran <www.1174965274@qq.com>
2019-02-28 14:09:45 +0800

    一

---
+++ communication/src/main/java/tech/wd/com/communication/relation/RelationFragment.java
@@ -0,0 +1,32 @@
+package tech.wd.com.communication.relation;
+
+import android.view.View;
+
+import tech.wd.com.common.base.BaseFragment;
+
+public class RelationFragment extends BaseFragment {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void success(Object object) {
+
+    }
+
+    @Override
+    protected void failed(String error) {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}
